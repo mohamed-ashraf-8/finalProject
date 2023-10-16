12c6c42ffe7671937728af06c49d3ebed11b0d37
Mohamed Ashraf <109896315+mohamed-ashraf-8@users.noreply.github.com>
2023-10-16 14:38:31 +0300

    Add files via upload

---
+++ web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('login',[AuthController::class,'index'])->name('login');
+Route::post('login',[AuthController::class,'login'])->name('login');
+Route::get('register',[AuthController::class,'register_view'])->name('register');
+Route::post('register',[AuthController::class,'register'])->name('register');
+Route::get('home', [AuthController::class, 'home'])->name('product.home');
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/product/{product}/destroy', [ProductController::class, 'destroy'])->name('product.destroy');
